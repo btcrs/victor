3fa4e5a944c62368654d55d49f5786be586b6e4c
Ben Carothers <btcrs@icloud.com>
2017-02-12 16:35:51 -0500

    Update 2017-02-12T21:35:49.141Z

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 'use strict';
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
+angular.module('victor', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+  'victor.login',
+  'victor.dashboard',
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
-
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.otherwise({redirectTo: '/login'});
 }]);
