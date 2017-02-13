3fea47d439332e346d252946194419092b0e63b4
Ben Carothers <btcrs@icloud.com>
2017-02-12 22:18:51 -0500

    Update 2017-02-13T03:18:49.786Z

--- login/login.js
+++ login/login.js
@@ -11,14 +11,14 @@ angular.module('victor.login', ['ngRoute'])
 
 .controller('LoginController', function($scope, $http, $location) {
 
-  var authenticationEndpoint = "https://43kmoq1cf2.execute-api.us-east-1.amazonaws.com/dev/authentication/"
+  $scope.authenticationEndpoint = "https://43kmoq1cf2.execute-api.us-east-1.amazonaws.com/dev/authentication/"
 
   $scope.submit = function() {
      $location.path('/dashboard');
   };
 
   $scope.login = function(provider){
-     $http.get(authenticationEndpoint + 'signin/' + provider)
+     window.location = ($scope.authenticationEndpoint + 'signin/' + provider)
   }
 
 });
