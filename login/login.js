37c75940ec781b8d8a9196b5565a53d4d0863044
Ben Carothers <btcrs@icloud.com>
2017-02-12 20:52:29 -0500

    Update 2017-02-13T01:52:27.349Z

--- login/login.js
+++ login/login.js
@@ -9,8 +9,16 @@ angular.module('victor.login', ['ngRoute'])
   });
 }])
 
-.controller('LoginController', function($scope, $location) {
+.controller('LoginController', function($scope, $http, $location) {
+
+  var authenticationEndpoint = "https://43kmoq1cf2.execute-api.us-east-1.amazonaws.com/dev/authentication/"
+
   $scope.submit = function() {
      $location.path('/dashboard');
-   };
+  };
+
+  $scope.login = function(provider){
+     $http.get(authenticationEndpoint + '/signin/' + provider)
+  }
+
 });
