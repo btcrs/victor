558483a0cfbbe14bb90cf5a5b8cb9f7db111c069
Ben Carothers <btcrs@icloud.com>
2017-04-30 14:57:41 -0400

    Expose control functionality

--- app/control/control.js
+++ app/control/control.js
@@ -11,5 +11,24 @@ angular.module('victor.control', ['ngRoute', 'angular.filter'])
 
   .controller('ControlController', function($scope, $interval, $timeout, $http, moment) {
     var vm = this
+    vm.submitForm = function() {
+     var data = {
+        "command": vm.command,
+        "parameter": vm.parameter,
+        "otp": vm.otp
+     }
+     console.log(data)
+     var config = {
+       headers : {
+         'Content-Type': 'application/json'
+       }
+     }
 
+     $http.post('https://aadrsu3hne.execute-api.us-east-1.amazonaws.com/dev/datum/relay.relay', data, config)
+     .success(function (data, status, headers, config) {
+         $scope.PostDataResponse = data;
+     }).error(function (data, status, header, config) {
+         console.log("Bad Response")
+     });
+    };
   });
